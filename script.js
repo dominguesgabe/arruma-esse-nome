7128e0e19a6cefb105ab5b46a4a0fee0874acfcf
gabriel <gabrieldomingues522@gmail.com>
2022-03-27 23:39:09 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const textField = document.querySelector('.text-field');
+// const insertHotfixBtn = document.querySelector('.hotfix-btn');
+// const insertFeatureBtn = document.querySelector('.feature-btn');
+// const generate = document.querySelector('.generate');
+// const copy = document.querySelector('.copy');
+
+// window.onload = () => {
+
+//     const hotfix = 'hotfix/CLI-';
+//     const feature = 'feature/CLI-';
+    
+//     function insert(caller) {
+        
+//         if (caller == 'hotfix') {
+//             textField = hotfix;
+//         } else {
+//             textField = feature;
+//         }
+//     }
+//     insertHotfixBtn.onclick = console.log(feature)
+
+// }
+
+var a = document.querySelector('.hotfix-btn')
+
+
+
+a.addEventListener('click', console.log('a'))
