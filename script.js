1f962bcbffb9e5f472de73757b904ee437e0e76e
gabriel <gabrieldomingues522@gmail.com>
2022-03-30 00:32:21 -0300

    adicionando alerts, feature e cle na string e funcao de copy

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const inputField = document.querySelector('#input-field');
 const outputField = document.querySelector('#output-field');
 const hotfixBtn = document.querySelector('#hotfix');
 const featureBtn = document.querySelector('#feature');
+const errorAlert = document.querySelector('#error-alert');
+const successAlert = document.querySelector('#success-alert');
+const container = document.querySelector('.container');
+const copyBtn = document.querySelector('#copy');
+
+const regexCLI = /^[A-Z]{3}[-][0-9]{4}[-]/;
 
 const accentsMap = new Map([
     ["a", "Á|À|Ã|Â|Ä"],
@@ -24,17 +30,45 @@ const reducer = (acc, [key]) => acc.replace(new RegExp(accentsMap.get(key), "g")
 const removeAccents = (text) => [...accentsMap].reduce(reducer, text);
 
 function insert(param) {
+    const caller = param.target.id;
     let input = removeAccents(inputField.value);
-     input = input.replace(/[^a-zA-Z ]/g, "");
-     input = input.split(' ');
-     input = input.join('-');
+    
+    if(regexCLI.test(input)) {
+        if(errorAlert.classList.contains('modalActive')) {
+            errorAlert.classList.toggle('modalActive');
+            container.classList.toggle('modalActive');
+        };
 
-    outputField.value = input;
-    console.log(param.target.id)
+        input = input.replace(/[^a-zA-Z0-9 -]/g, "");
+        input = input.split(' ');
+        input = input.join('-');
+        input = `${caller}/${input}` 
+
+        outputField.value = input;
+    } else {
+        if(!errorAlert.classList.contains('modalActive')) {
+            errorAlert.classList.toggle('modalActive');
+            container.classList.toggle('modalActive');
+        };
+    };
+
+
+    
+}
+
+function copy() {
+    outputField.select();
+    document.execCommand('copy');
+
+    if(!successAlert.classList.contains('modalActive')) {
+        successAlert.classList.toggle('modalActive');
+        container.classList.toggle('modalActive');
+    };
 }
 
 hotfixBtn.addEventListener('click', insert)
 featureBtn.addEventListener('click', insert)
+copyBtn.addEventListener('click', copy)
 
-// quando fazer imagens do tipo "webp" não permitir o uso da extensão!$
+// cLi-1233 quando fazer imagens do tipo "webp" não pe!rmitir o uso da extensão$
 
