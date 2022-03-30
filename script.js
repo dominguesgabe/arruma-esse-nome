dd0e19ad0bf8ad10857616065afa268564e4c0a0
gabriel <gabrieldomingues522@gmail.com>
2022-03-29 21:27:26 -0300

    melhorando o processamento da string

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 const inputField = document.querySelector('#input-field');
-let outputField = document.querySelector('#output-field');
-const insertHotfixBtn = document.querySelector('#hotfix-btn');
+const outputField = document.querySelector('#output-field');
+const hotfixBtn = document.querySelector('#hotfix');
+const featureBtn = document.querySelector('#feature');
 
 const accentsMap = new Map([
-    ["A", "Á|À|Ã|Â|Ä"],
+    ["a", "Á|À|Ã|Â|Ä"],
     ["a", "á|à|ã|â|ä"],
-    ["E", "É|È|Ê|Ë"],
+    ["e", "É|È|Ê|Ë"],
     ["e", "é|è|ê|ë"],
-    ["I", "Í|Ì|Î|Ï"],
+    ["i", "Í|Ì|Î|Ï"],
     ["i", "í|ì|î|ï"],
-    ["O", "Ó|Ò|Ô|Õ|Ö"],
+    ["o", "Ó|Ò|Ô|Õ|Ö"],
     ["o", "ó|ò|ô|õ|ö"],
-    ["U", "Ú|Ù|Û|Ü"],
+    ["u", "Ú|Ù|Û|Ü"],
     ["u", "ú|ù|û|ü"],
-    ["C", "Ç"],
+    ["c", "Ç"],
     ["c", "ç"],
-    ["N", "Ñ"],
+    ["n", "Ñ"],
     ["n", "ñ"]
   ]);
   
-const reducer = (acc, [key]) => {
-    return acc.replace(new RegExp(accentsMap.get(key), "g"), key); 
-};
+const reducer = (acc, [key]) => acc.replace(new RegExp(accentsMap.get(key), "g"), key); 
 const removeAccents = (text) => [...accentsMap].reduce(reducer, text);
 
-function insert() {
-    let input = inputField.value.split(' ');
-    let processingString = input.join('-');
-    let processedString = removeAccents(processingString);
-    
-    outputField.value = processedString;
+function insert(param) {
+    let input = removeAccents(inputField.value);
+     input = input.replace(/[^a-zA-Z ]/g, "");
+     input = input.split(' ');
+     input = input.join('-');
+
+    outputField.value = input;
+    console.log(param.target.id)
 }
 
-insertHotfixBtn.addEventListener('click', insert)
+hotfixBtn.addEventListener('click', insert)
+featureBtn.addEventListener('click', insert)
 
 // quando fazer imagens do tipo "webp" não permitir o uso da extensão!$
 
